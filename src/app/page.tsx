2ac908c30ac9b49872f3163f24dda20c36110df1
marlissonsilva <marlyssonsilva005@gmail.com>
2025-03-04 13:12:55 -0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { IconArrowRight } from '@tabler/icons-react'
+
+import { Button } from '@/components/ui/button'
+import { Header } from '@/components/Header'
+
+import styles from './page.module.css'
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl text-center">Card Management</h1>
-    </div>
+    <>
+      <Header />
+      <main className={styles.wrapper}>
+        <div className={styles.copy}>
+          <h2>
+            Para você que compartilha seu cartão de crédito e sempre precisa
+            calcular os valores de cada pessoa, temos a solução ideal para
+            facilitar sua vida! 🚀
+          </h2>
+          <Link href={'/dashboard'}>
+            <Button size={'lg'}>
+              Login
+              <IconArrowRight />
+            </Button>
+          </Link>
+        </div>
+        <div className={styles.containerImage}>
+          <Image
+            src={'/Invoice-bro.svg'}
+            width={800}
+            height={800}
+            alt="Image"
+          />
+        </div>
+      </main>
+    </>
   )
 }
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./logo.module.css"
+
+export function Logo() {
+  return (
+    <div className={styles.logo}>
+      <h1 className={styles.logo__text}>CardControl</h1>
+    </div>
+  )
+}
