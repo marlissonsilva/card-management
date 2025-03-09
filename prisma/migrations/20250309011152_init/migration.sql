0fff03747da68409dda0282c4369f2ac58c4458f
marlissonsilva <marlyssonsilva005@gmail.com>
2025-03-08 22:14:54 -0300

    build: change database

--- prisma/migrations/20250309011152_init/migration.sql
+++ prisma/migrations/20250309011152_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Purchase" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "responsible" TEXT NOT NULL,
-    "amount" REAL NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
     "description" TEXT NOT NULL,
     "status" BOOLEAN NOT NULL DEFAULT false,
     "installments" INTEGER NOT NULL,
-    "dateOfPurchase" DATETIME NOT NULL
+    "dateOfPurchase" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Purchase_pkey" PRIMARY KEY ("id")
 );
--- src/app/query/Purchase.ts
+++ src/app/query/Purchase.ts
@@ -1,13 +1,8 @@
 'use server'
 import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = global as unknown as { prisma?: PrismaClient }
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
 import { Purchase } from '../data/models/Purchase'
+
+const prisma = new PrismaClient()
 
 export async function createPurchase(data: Purchase) {
   const result = await prisma.purchase.create({ data })
