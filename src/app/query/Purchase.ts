25b11ef94ea0f4ae43f0df256bed45b204a643af
marlissonsilva <marlyssonsilva005@gmail.com>
2025-03-08 21:49:39 -0300

    fix: prisma client

--- src/app/query/Purchase.ts
+++ src/app/query/Purchase.ts
@@ -1,13 +1,9 @@
 'use server'
-import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+import prisma from "@/lib/prisma"
+import { Purchase } from "../data/models/Purchase"
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-import { Purchase } from '../data/models/Purchase'
 
 export async function createPurchase(data: Purchase) {
   const result = await prisma.purchase.create({ data })
