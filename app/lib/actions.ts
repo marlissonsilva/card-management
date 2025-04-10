b45506c7d57659a1dc9874c14f5dd099f447a4e7
marlissonsilva <marlyssonsilva005@gmail.com>
2025-04-10 17:39:26 -0300

    feat: actions and utils

---
+++ app/lib/actions.ts
@@ -0,0 +1,177 @@
+'use server'
+import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { prisma } from './prisma'
+
+const FormSchema = z.object({
+  id: z.string(),
+  responsible: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Valor tem que ser maior que R$: 0,00.' }),
+  description: z.string().min(5, 'Descrição deve ter pelo menos 5 caracteres'),
+  dateOfPurchase: z
+    .string()
+    .transform((val) => new Date(val))
+    .refine((date) => !isNaN(date.getTime()), {
+      message: 'Selecione uma data válida'
+    }),
+  installments: z.coerce
+    .number()
+    .int()
+    .min(1, 'Parcelas devem ser maior que 0')
+    .max(12, 'Parcelas devem ser menor que 12')
+})
+
+export type State = {
+  errors?: {
+    responsible?: string[] | undefined
+    amount?: string[] | undefined
+    description?: string[] | undefined
+    dateOfPurchase?: string[] | undefined
+    installments?: string[] | undefined
+  }
+  message?: string | undefined
+}
+
+const CreatePurchase = FormSchema.omit({ id: true })
+
+export async function createPurchase(
+  state: State,
+  formData: FormData
+): Promise<State> {
+  const validatedFields = CreatePurchase.safeParse({
+    responsible: formData.get('responsible'),
+    amount: formData.get('amount'),
+    description: formData.get('description'),
+    dateOfPurchase: formData.get('dateOfPurchase'),
+    installments: formData.get('installments')
+  })
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors)
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Falha ao criar compra'
+    }
+  }
+
+  try {
+    await prisma.purchase.create({
+      data: {
+        ...validatedFields.data
+      }
+    })
+  } catch (error) {
+    return {
+      message: `Database error: Falha ao criar compra.${error}`
+    }
+  }
+
+  revalidatePath('/dashboard/')
+  redirect('/dashboard/compras')
+}
+
+const ITEMS_PER_PAGE = 8
+export async function listPurchase(query: string, currentPage: number) {
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE
+
+  const purchases = await prisma.purchase.findMany({
+    where: {
+      OR: [
+        {
+          responsible: {
+            contains: query,
+            mode: 'insensitive'
+          }
+        }
+      ]
+    },
+    skip,
+    take: ITEMS_PER_PAGE,
+    orderBy: {
+      dateOfPurchase: 'desc'
+    }
+  })
+  return purchases
+}
+
+export async function fetchPurchasePages(query: string) {
+  try {
+    const totalInvoices = await prisma.purchase.count({
+      where: {
+        OR: [
+          {
+            responsible: {
+              contains: query,
+              mode: 'insensitive'
+            }
+          }
+        ]
+      }
+    })
+
+    const totalPages = Math.ceil(totalInvoices / ITEMS_PER_PAGE)
+    return totalPages
+  } catch (error) {
+    console.error('Database Error:', error)
+    throw new Error('Failed to fetch total number of invoices.')
+  }
+}
+
+export async function getResponsibles() {
+  return await prisma.purchase.findMany({
+    distinct: ['responsible']
+  })
+}
+
+export async function deletePurchase(id: string) {
+  prisma.purchase.delete({
+    where: {
+      id
+    }
+  })
+  revalidatePath('/dashboard/compras')
+}
+
+export async function fetchCardData() {
+  try {
+    const [amount, pending, totalResponsible, purchases] = await Promise.all([
+      prisma.purchase.aggregate({
+        _sum: {
+          amount: true
+        }
+      }),
+
+      prisma.purchase.aggregate({
+        _sum: {
+          amount: true
+        },
+        where: {
+          status: false
+        }
+      }),
+
+      prisma.purchase.findMany({
+        distinct: ['responsible']
+      }),
+
+      prisma.purchase.count()
+    ])
+
+    const totalAmount = Number(amount._sum.amount?.toFixed(2) || 0)
+    const totalPending = Number(pending._sum.amount?.toFixed(2) || 0)
+    const totalPurchases = purchases
+
+    return {
+      totalAmount,
+      totalPending,
+      totalResponsible,
+      totalPurchases
+    }
+  } catch (error) {
+    console.error('Database Error:', error)
+    throw new Error('Failed to fetch card data.')
+  }
+}
---
+++ app/lib/utils.ts
@@ -0,0 +1,50 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+export default function formatCurrency(value: number | string) {
+  return value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2
+  })
+}
+
+export const formatDateToLocal = (dateStr: Date, locale: string = 'pt-BR') => {
+  const date = new Date(dateStr)
+  const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }
+  const formatter = new Intl.DateTimeFormat(locale, options)
+  return formatter.format(date)
+}
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1)
+  }
+
+  if (currentPage <= 3) {
+    return [1, 2, 3, '...', totalPages - 1, totalPages]
+  }
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages]
+  }
+
+  return [
+    1,
+    '...',
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    '...',
+    totalPages
+  ]
+}
