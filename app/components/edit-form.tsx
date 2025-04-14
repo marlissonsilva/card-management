80e0b8de6392b1ef890a5a625651f3fce1c7d487
marlissonsilva <marlyssonsilva005@gmail.com>
2025-04-14 20:46:35 -0300

    fix: update responsible type in edit form to use Purchase array for improved data structure

--- app/components/edit-form.tsx
+++ app/components/edit-form.tsx
@@ -39,7 +39,7 @@ export default function FormEdit({
   purchases,
 }: {
   purchases: Purchase
-  responsible: string[]
+  responsible: Purchase[]
 }) {
   const initialState: State = { message: undefined, errors: {} }
   const updatePurchaseById = updatePurchase.bind(null, purchases.id)
